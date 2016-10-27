5be949c1004a624d140247dd31e3764b5decc7e5
Tina Sharma <tina_2212@hotmail.com>
2016-10-26 23:35:03 -0700

    first commit

---
+++ ProductsApp/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+/*using Microsoft.Framework.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+//using LogLevel = Microsoft.Framework.Logging.LogLevel;
+using Microsoft.Azure;*/
+
+namespace ProductsApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+
+        /*CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("<tinasharma>_AzureStorageConnectionString"));
+
+        // Create the table client.
+        CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+        // Get a reference to a table named "productTable"
+        CloudTable table = tableClient.GetTableReference("productTable");
+
+        // Create the CloudTable if it does not exist
+        await table.CreateIfNotExistsAsync();*/
+    }
+
+    
+}
